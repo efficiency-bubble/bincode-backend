05cd6c14e0dfa68c1ef30c7c7590f67645a84617
maf <marralesfios@gmail.com>
2025-07-17 18:28:12 +0800

    ljf optimization for unconditional block returns

--- src/implementation/ljf.cpp
+++ src/implementation/ljf.cpp
@@ -2,21 +2,33 @@
 #include<bbe/inter/ljf.hpp>
 #include<ranges>
 #include<format>
+#include<print>
 namespace bbe::targets::ljf::impl{
-    struct BlockTransfer{
-        std::vector<std::pair<std::uint32_t,std::uint32_t>> moves;
-        std::uint32_t to;
-        BlockTransfer(std::uint32_t to) : moves(), to(to){}
-    };
+    using BlockTransfer = std::vector<std::pair<std::uint32_t,std::uint32_t>>;
+    void populate_tt(BlockTransfer& tt,const ssa::BasicBlock& src,const ssa::BasicBlock& dst){
+        for(const auto& [n,v] : dst.imports()){
+            tt.emplace_back(src.nametable().at(n),v);
+        }
+    }
+    void write_transfers(std::vector<ssa::Instruction>& ins,const BlockTransfer& tt){
+        for(const auto& [from,to] : tt){
+            ins.emplace_back(ssa::Operation::MOV,to,std::vector<std::uint32_t>{from});
+        }
+    }
     ProcedureIC::ProcedureIC(const ssa::ProcedureIC& ssaf){
+        std::vector<std::pair<std::uint32_t,BlockTransfer>> branched_transfer_table;
         std::vector<BlockTransfer> transfer_table;
         std::vector<std::vector<std::uint32_t>> transfer_id(ssaf.blocks().size());
         for(const auto& [out_ids,block] : std::views::zip(transfer_id,ssaf.blocks())){
-            for(const std::uint32_t dst : block.retlocs()){
-                out_ids.emplace_back(transfer_table.size()+ssaf.blocks().size());
-                BlockTransfer& entry = transfer_table.emplace_back(dst);
-                for(const auto& [n,v] : ssaf.blocks()[dst].imports()){
-                    entry.moves.emplace_back(block.nametable().at(n),v);
+            if(block.retcond()==block.NCOND){
+                if(!block.retlocs().empty()){
+                    out_ids.emplace_back(transfer_table.size());
+                    populate_tt(transfer_table.emplace_back(),block,ssaf.blocks()[block.retlocs().front()]);
+                }
+            }else{
+                for(const std::uint32_t dst : block.retlocs()){
+                    out_ids.emplace_back(branched_transfer_table.size()+ssaf.blocks().size());
+                    populate_tt(branched_transfer_table.emplace_back(std::piecewise_construct,std::forward_as_tuple(dst),std::tuple<>()).second,block,ssaf.blocks()[dst]);
                 }
             }
         }
@@ -25,19 +37,18 @@ namespace bbe::targets::ljf::impl{
             _instructions.append_range(block.instructions());
             if(block.retcond() == block.NCOND){
                 if(!out_ids.empty()){
-                    [[assume(out_ids.size()==1uz)]];
-                    _instructions.emplace_back(ssa::Operation::JMP,0,out_ids);
+                    [[assume(block.retlocs().size()==1uz)]];
+                    write_transfers(_instructions,transfer_table[out_ids.front()]);
+                    _instructions.emplace_back(ssa::Operation::JMP,0,block.retlocs());
                 }
             }else{
                 _instructions.emplace_back(ssa::Operation::JMP,block.retcond(),out_ids);
             }
         }
-        for(const BlockTransfer& trans : transfer_table){
+        for(const auto& [next,trans] : branched_transfer_table){
             _labels.emplace_back(_instructions.size());
-            for(const auto& [from,to] : trans.moves){
-                _instructions.emplace_back(ssa::Operation::MOV,to,std::vector<std::uint32_t>{from});
-            }
-            _instructions.emplace_back(ssa::Operation::JMP,0,std::vector<std::uint32_t>{trans.to});
+            write_transfers(_instructions,trans);
+            _instructions.emplace_back(ssa::Operation::JMP,0,std::vector<std::uint32_t>{next});
         }
     }
 }
@@ -46,10 +57,11 @@ namespace bbe::inter::ljf::impl{
         FunctionLocals lc;
         std::uint32_t pc = 0;
         while(true){
+            // std::print("{}: ",pc);
             // for(const auto& [k,v] : lc){
-            //     std::cout << k << '=' << v.value().tell() << " ; ";
+            //     std::print("{} = {} ; ",k,v.value().tell());
             // }
-            // std::cout << '\n';
+            // std::println();
             if(pc >= fn.instructions().size()){
                 throw std::logic_error("inter::ljf::run(): Execution reached end of code without return");
             }
@@ -92,10 +104,10 @@ namespace bbe::inter::ljf::impl{
                         const Value& cond = lc[ins.dst];
                         switch(cond.index()){
                             case Value::index_of<boolv>:
-                                pc = fn.labels()[cond.get<boolv>().value];
+                                pc = fn.labels()[ins.src[cond.get<boolv>().value]];
                                 break;
                             case Value::index_of<uint32v>:
-                                pc = fn.labels()[cond.get<uint32v>().value];
+                                pc = fn.labels()[ins.src[cond.get<uint32v>().value]];
                                 break;
                             default:
                                 throw std::logic_error(std::format("inter::ljf::run(): value of type {} cannot be used as a branch condition"sv,ins.src.front()));
