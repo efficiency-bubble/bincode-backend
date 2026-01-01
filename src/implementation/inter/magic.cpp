17080e41df1e9f1fed22e71bdde162ccc4f8f525
maf <marralesfios@gmail.com>
2026-01-02 02:55:11 +0800

    equality comparison

--- src/implementation/inter/magic.cpp
+++ src/implementation/inter/magic.cpp
@@ -7,11 +7,13 @@ namespace bbe::inter::impl{
     Value cmag(std::uint32_t magic,const Value& arg){
         const auto& packv = arg.get<pack>().values;
         switch(magic){
-            case 10: // add
+            case 10: // add32
                 return uint32v(packv.front().get<uint32v>().value+packv[1uz].get<uint32v>().value);
             case 25: // pru32
                 std::print("{}",packv.front().get<uint32v>().value);
                 return {};
+            case 50: // eq32
+                return boolv(packv.front().get<uint32v>().value == packv[1uz].get<uint32v>().value);
             default:
                 throw std::logic_error(std::format("inter::cmag(): Unknown magic function {}"sv,magic));
         }
